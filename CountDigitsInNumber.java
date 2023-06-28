fc36893572b279118af022dc50451535924a229c
Rushikesh Lande <rushilande999@gmail.com>
2023-06-28 17:09:40 +0530

    assessment 27-06-2023

---
+++ CountDigitsInNumber.java
@@ -0,0 +1,13 @@
+/* Java Program to Count the Number of Digits in a Number.*/
+class CountDigitsInNumber{
+    public static void main(String args[]){
+     
+        long a=23458;
+
+       String str=Long.toString(a);
+
+            System.out.println("the count of a digits in a given number is : "+str.length());
+
+          
+
+}}
