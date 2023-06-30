250e05e92b4d64a2abccb0823b6e7026ca1d6f9e
Amar <pawarpatilamar@gmail.com>
2023-06-30 13:51:53 +0530

    control statements /  Check Leap Year Program

---
+++ CheckLeapYear.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class CheckLeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        boolean isLeapYear = checkLeapYear(year);
+        //case 1
+
+        if (isLeapYear) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+
+    public static boolean checkLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
