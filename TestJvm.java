3dc614f861f06a0b50f915743981bc4b57b3ce93
unknown <dev.sushensable@gmail.com>
2023-07-08 09:57:31 +0530

    first cmt

---
+++ TestJvm.java
@@ -0,0 +1,20 @@
+public class TestJvm{
+	static int num=5;
+	static String name="Ram";
+	
+	public static void main(String[] ar){
+		for(String s:ar){
+		System.out.println(s);
+		}
+		
+		
+		
+	}
+}
+
+class Student{
+	protected static int classId=50;
+	String name="student1";
+	private static int studnetCouont;
+	
+}
