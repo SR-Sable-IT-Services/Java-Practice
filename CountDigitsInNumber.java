af28afd9e98a443e59d10b27b7a8a47fafce4f8f
Amarpawar2 <pawarpatilamar@gmail.com>
2023-07-11 08:03:06 +0530

    control operation / check no of digits

---
+++ CountDigitsInNumber.java
@@ -0,0 +1,13 @@
+
+class CountDigitsInNumber{
+    public static void main(String args[]){
+     
+        long a=8467829;
+
+       String str=Long.toString(a);
+
+            System.out.println("the count of a digits in a given number is : "+str.length());
+
+          
+
+}}
