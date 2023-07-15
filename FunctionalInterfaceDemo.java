8e2b69988367cfb518d0195669445440cf60093e
Rushikesh Lande <rushilande999@gmail.com>
2023-07-15 05:52:57 +0530

    Fuctional Interface // Fuctional Interface Program

---
+++ FunctionalInterfaceDemo.java
@@ -0,0 +1,39 @@
+/* assessment 15-07-2023
+
+*  write an functional interface and create object for it using anonymous class.
+   write an functional interface and create object for it using lambda expression.
+*/
+@FunctionalInterface
+interface FunctionalDemo {  
+    int mul(int a, int b); // abstract method
+
+    String toString(); //  toString method of Object class
+
+    int hashCode();  //   hashCode method of Object Class
+
+    default void dmeth() {  // default method
+        pmeth();
+    }
+
+    private void pmeth() {   // private method 
+        System.out.println("private method");
+    }
+
+    static void smeth() {  // static method
+        System.out.println("Static method");
+    }
+}
+
+class FunctionalInterfaceDemo {
+    public static void main(String args[]) {
+        FunctionalDemo obj = new FunctionalDemo() {
+            public int mul(int a, int b) {
+                return a * b;
+            }
+        };
+
+        System.out.println(obj.mul(3, 12));
+
+        obj.dmeth();
+    }
+}
